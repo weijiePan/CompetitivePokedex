cd6c71f322dbbc55b5d88b021bf624e6d4f8f1e6
weijiePan <97200483+weijiePan@users.noreply.github.com>
2025-08-16 07:36:48 -0400

    faq

--- my_app/app/page.tsx
+++ my_app/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Pokedex from "./components/pokemonDisplay/Pokedex"
-import OneColumnPage from "./components/pageLayout/OneColumnPage";
+import OneColumnPage from "./components/structure/pageLayout/OneColumnPage";
 import ProductBanner from "./sections/ProductBanner";
-import SplitSection from "./components/sectionLayout/SplitSection";
-import NavBtn from "./components/buttons/NavBtn";
+import SplitSection from "./components/structure/sectionLayout/SplitSection";
+import NavBtn from "./components/interactivity/buttons/NavBtn";
+import FaqBar from "./components/structure/sectionLayout/Faq/FaqBar";
 import "./home.css";
+import Faq from "./components/structure/sectionLayout/Faq/Faq";
+
 const pokedexAbout = {
   img:"/assets/pages/pokedex.gif",
   imgRes:{height:1920 ,width:1032},
@@ -24,18 +27,34 @@ const teamAbout = {
   isImgLeft:false,
 }
 
+const faq1 = {
+  question:"what generation and format is covered",
+  answer:"1 - 9 and VGC"
+}
 
+const faq2 = {
+  question:"How is the data collected?",
+  answer:"The data comes from sources including PokeApi, smogon... "
+}
+
+const faq3 = {
+  question:"Where can I find the source code for this website",
+  answer:"https://github.com/weijiePan/CompetitivePokedex"
+}
+
+const faqs = [faq1, faq2, faq3];
 export default function Home(props:any) {
   const nodes = [
     {node:<ProductBanner title="competitive pokedex"></ProductBanner>, height: "40%" },
     {node:<SplitSection {...pokedexAbout}></SplitSection>, height:"40%"},
-    {node:<SplitSection {...teamAbout}></SplitSection>, height:"40%"},
-    {node:<NavBtn className="pokedexJoin"  text = "start now" action = {""}></NavBtn>, height:"10%"},
+    {node:<SplitSection {...teamAbout}></SplitSection>, height:"45%"},
+    {node:<NavBtn className="pokedexJoin"  text = "start now" action = {""}></NavBtn>, height:"15%", clsName:"joinDiv"},
+    {node:<Faq infos = {faqs}></Faq>, height:"40%", clsName:"faq"},
   ]
   return (
     <>
       <OneColumnPage sections = {nodes} ></OneColumnPage>
-        
+      
 
     </>
     
