e45c8f70dc78dc4c60a258c84ebcf146bf78c523
weijiePan <97200483+weijiePan@users.noreply.github.com>
2025-08-16 23:18:28 -0400

    incorrect case in import statement fix

--- my_app/app/components/interactivity/buttons/RectangleBtn.tsx
+++ my_app/app/components/interactivity/buttons/RectangleBtn.tsx
@@ -1,5 +1,5 @@
 import Link from "next/navigation"
-import "./rectangleBtn.css"
+import "./RectangleBtn.css"
 import { MouseEventHandler } from "react";
 function RectangleBtn({ className, text, action}: {className:string, text:string, action: MouseEventHandler}) {//action takes either a link or a callback
     return(
--- my_app/app/components/structure/pageLayout/OneColumnPage.tsx
+++ my_app/app/components/structure/pageLayout/OneColumnPage.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react"
-import "./oneColumnPage.css"
+import "./OneColumnPage.css"
 export default function OneColumnPage({sections}: {sections?:{node:ReactNode, height:string, clsName?:string}[]}){
     const divSections = sections?.map((section) =>{
         return(<div key = {crypto.randomUUID()} className = {`section ${section.clsName?section.clsName:""}`} style={{height:section.height}}>{section.node}</div>);
